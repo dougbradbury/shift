83e2310048df9db9c2d55f7370513dd44017fa29
dougbradbury <dbradbury@oneworldcoders.com>
2020-06-16 06:05:09 -0500

    implementes gear ratio for

--- src/gear_ratio.c
+++ src/gear_ratio.c
@@ -2,6 +2,6 @@
 
 float GearRatio_for(int front_crank_tooth_count, int rear_cassette_tooth_count)
 {
-  return 1.579;
+  return (float) front_crank_tooth_count / (float) rear_cassette_tooth_count;
 }
 
--- test/test_gear_ratio.c
+++ test/test_gear_ratio.c
@@ -1,3 +1,4 @@
+#define UNITY_FLOAT_PRECISION 0.001f
 #include "unity.h"
 
 #include "gear_ratio.h"
@@ -11,7 +12,8 @@ void tearDown(void)
 }
 
 //output ratio = tooth count on selected front cog / tooth count on selected rear cog
-void test_gear_ratio_NeedToImplement(void)
+void test_gear_ratio_for(void)
 {
-    TEST_ASSERT_EQUAL_FLOAT(1.579, GearRatio_for(30, 19));
+    TEST_ASSERT_EQUAL_FLOAT(1.578947, GearRatio_for(30, 19));
+    TEST_ASSERT_EQUAL_FLOAT(2.375, GearRatio_for(38, 16));
 }
